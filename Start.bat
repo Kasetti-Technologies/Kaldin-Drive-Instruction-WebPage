01b787812ddbd0fa87617c31c53a0eab96f014c1
Akhilesh HN <60725930+AkhileshHN@users.noreply.github.com>
2025-07-23 15:56:40 +0530

    Add exam instructions page and Windows start script
    Introduces public.html with detailed exam instructions and a Start.bat script to launch the exam portal in Microsoft Edge kiosk mode. These files support exam setup and provide guidance for candidates on exam day.

---
+++ Start.bat
@@ -0,0 +1,2 @@
+@echo off
+start msedge.exe --kiosk 192.168.1.221:8080/kaldin --edge-kiosk-type=fullscreen --no-first-run --disable-pinch --disable-translate --noerrdialogs --incognito --disable-features=Translate,IsolateOrigins,site-per-process,msDownloadsHub
